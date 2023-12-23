6cea92ea51c199e5e9f5b2162b256b2213c19218
Tianshi Che <43124213+tzc0029@users.noreply.github.com>
2023-12-22 20:41:47 -0600

    Add more examples

--- example_gpt2.sh
+++ example_gpt2.sh
@@ -1,6 +1,6 @@
 export GLUE_DIR=/datasets/
-export TASK_NAME=rte # options: boolq, cola, mpqa, mr, mrpc, qnli, rte, sst-2, subj, trec
-export MODEL_NAME_OR_PATH=roberta-large # options: roberta-large, gpt2-large, openlm-research/open_llama_3b, openlm-research/open_llama_7b
+export TASK_NAME=mrpc # options: boolq, cola, mpqa, mr, mrpc, qnli, rte, sst-2, subj, trec
+export MODEL_NAME_OR_PATH=gpt2-large # options: roberta-large, gpt2-large, openlm-research/open_llama_3b, openlm-research/open_llama_7b
 NUMCLIENTS=100
 NUMEPOCHS=100
 SAMPLERATE=0.1
@@ -15,8 +15,8 @@ LR=4e-2
 
 # Step 1: Use the following code to obtain the importance ranking of prompt layers:
 export CUDA_VISIBLE_DEVICES=0
-python encoder-only-roberta-large/run_getscore.py \
-      --model_type roberta \
+python decoder-only-gpt2/run_getscore.py \
+      --model_type gpt2 \
       --model_name_or_path $MODEL_NAME_OR_PATH \
       --task_name $TASK_NAME \
       --do_train \
@@ -26,7 +26,7 @@ python encoder-only-roberta-large/run_getscore.py \
       --data_dir "$GLUE_DIR" \
       --log_dir ./logs/ \
       --output_dir ./ckpts/ \
-      --max_seq_length 512 \
+      --max_seq_length 256 \
       --per_gpu_train_batch_size 8 \
       --per_gpu_eval_batch_size 8 \
       --learning_rate $LR \
@@ -39,7 +39,7 @@ python encoder-only-roberta-large/run_getscore.py \
       --logging_steps 100 \
       --num_train_epochs $NUMEPOCHS  \
       --warmup_rate 0.06 \
-      --num_prompt_tokens 20 \
+      --num_prompt_tokens 5 \
       --prompt_layer_list None \
       --evaluate_during_training \
       --overwrite_output_dir \
@@ -51,8 +51,8 @@ python encoder-only-roberta-large/run_getscore.py \
 
 # Step 2: Use the following code to calculate the minimum remaining prompt parameter ratio:
 export CUDA_VISIBLE_DEVICES=0
-python encoder-only-roberta-large/run_getpr_perclient.py \
-      --model_type roberta \
+python decoder-only-gpt2/run_getpr_perclient.py \
+      --model_type gpt2 \
       --model_name_or_path $MODEL_NAME_OR_PATH \
       --task_name $TASK_NAME \
       --do_train \
@@ -62,7 +62,7 @@ python encoder-only-roberta-large/run_getpr_perclient.py \
       --data_dir "$GLUE_DIR" \
       --log_dir ./logs/ \
       --output_dir ./ckpts/ \
-      --max_seq_length 512 \
+      --max_seq_length 256 \
       --per_gpu_train_batch_size 8 \
       --per_gpu_eval_batch_size 8 \
       --learning_rate $LR \
@@ -75,7 +75,7 @@ python encoder-only-roberta-large/run_getpr_perclient.py \
       --logging_steps 100 \
       --num_train_epochs $NUMEPOCHS  \
       --warmup_rate 0.06 \
-      --num_prompt_tokens 20 \
+      --num_prompt_tokens 5 \
       --prompt_layer_list None \
       --evaluate_during_training \
       --overwrite_output_dir \
@@ -86,16 +86,14 @@ python encoder-only-roberta-large/run_getpr_perclient.py \
       --local_eps $LOCALEPS &
 
 # By combining the sorted layer indexes from Step 1 and the ratio calculated in Step 2, selected layers for efficient prompt tuning can be obtained
-# e.g.: 24 layers of roberta-large in sorted order: [9, 8, 11, 7, 10, 17, 18, 15, 16, 22, 12, 21, 13, 14, 6, 19, 20, 2, 5, 1, 4, 3, 0]    
-#       ratio calculated in Step 2: 50%
-#       efficient prompt tuning layers for roberta-large model on RTE dataset: [9, 8, 11, 7, 10, 17, 18, 15, 16, 22, 12, 21]
+# e.g.: efficient prompt tuning layers for gpt2 model on MRPC dataset: [2, 1, 3, 34, 33, 32, 31, 29, 30, 28, 0, 27, 4, 26, 25, 24, 23, 22]
 
 
 
 # Step 3: Specify efficient prompt tuning layers with parameter 'prompt_layer_list', and start tuning with the follwoing code
 export CUDA_VISIBLE_DEVICES=0
-python encoder-only-roberta-large/run_fed_pers_with_optim_v2.py \
-    --model_type roberta \
+python decoder-only-gpt2/run_fed_pers_with_optim_v2.py \
+    --model_type gpt2 \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --task_name $TASK_NAME \
     --do_train \
@@ -105,7 +103,7 @@ python encoder-only-roberta-large/run_fed_pers_with_optim_v2.py \
     --data_dir "$GLUE_DIR" \
     --log_dir ./logs/ \
     --output_dir ./ckpts/ \
-    --max_seq_length 512 \
+    --max_seq_length 256 \
     --per_gpu_train_batch_size 8 \
     --per_gpu_eval_batch_size 8 \
     --learning_rate $LR \
@@ -118,8 +116,8 @@ python encoder-only-roberta-large/run_fed_pers_with_optim_v2.py \
     --logging_steps 100 \
     --num_train_epochs $NUMEPOCHS  \
     --warmup_rate 0.06 \
-    --num_prompt_tokens 20 \
-    --prompt_layer_list "[9, 8, 11, 7, 10, 17, 18, 15, 16, 22, 12, 21]" \
+    --num_prompt_tokens 5 \
+    --prompt_layer_list "[2, 1, 3, 34, 33, 32, 31, 29, 30, 28, 0, 27, 4, 26, 25, 24, 23, 22]" \
     --evaluate_during_training \
     --overwrite_output_dir \
     --data_partition_method "dirichlet_label" \
--- example_llama.sh
+++ example_llama.sh
@@ -1,6 +1,6 @@
 export GLUE_DIR=/datasets/
-export TASK_NAME=rte # options: boolq, cola, mpqa, mr, mrpc, qnli, rte, sst-2, subj, trec
-export MODEL_NAME_OR_PATH=roberta-large # options: roberta-large, gpt2-large, openlm-research/open_llama_3b, openlm-research/open_llama_7b
+export TASK_NAME=mrpc # options: boolq, cola, mpqa, mr, mrpc, qnli, rte, sst-2, subj, trec
+export MODEL_NAME_OR_PATH=openlm-research/open_llama_7b # options: roberta-large, gpt2-large, openlm-research/open_llama_3b, openlm-research/open_llama_7b
 NUMCLIENTS=100
 NUMEPOCHS=100
 SAMPLERATE=0.1
@@ -15,8 +15,8 @@ LR=4e-2
 
 # Step 1: Use the following code to obtain the importance ranking of prompt layers:
 export CUDA_VISIBLE_DEVICES=0
-python encoder-only-roberta-large/run_getscore.py \
-      --model_type roberta \
+python decoder-only-llama/run_getscore.py \
+      --model_type llama \
       --model_name_or_path $MODEL_NAME_OR_PATH \
       --task_name $TASK_NAME \
       --do_train \
@@ -26,9 +26,9 @@ python encoder-only-roberta-large/run_getscore.py \
       --data_dir "$GLUE_DIR" \
       --log_dir ./logs/ \
       --output_dir ./ckpts/ \
-      --max_seq_length 512 \
-      --per_gpu_train_batch_size 8 \
-      --per_gpu_eval_batch_size 8 \
+      --max_seq_length 128 \
+      --per_gpu_train_batch_size 1 \
+      --per_gpu_eval_batch_size 2 \
       --learning_rate $LR \
       --personalization \
       --server_lr $SLR \
@@ -39,7 +39,7 @@ python encoder-only-roberta-large/run_getscore.py \
       --logging_steps 100 \
       --num_train_epochs $NUMEPOCHS  \
       --warmup_rate 0.06 \
-      --num_prompt_tokens 20 \
+      --num_prompt_tokens 5 \
       --prompt_layer_list None \
       --evaluate_during_training \
       --overwrite_output_dir \
@@ -51,8 +51,8 @@ python encoder-only-roberta-large/run_getscore.py \
 
 # Step 2: Use the following code to calculate the minimum remaining prompt parameter ratio:
 export CUDA_VISIBLE_DEVICES=0
-python encoder-only-roberta-large/run_getpr_perclient.py \
-      --model_type roberta \
+python decoder-only-llama/run_getpr_perclient.py \
+      --model_type llama \
       --model_name_or_path $MODEL_NAME_OR_PATH \
       --task_name $TASK_NAME \
       --do_train \
@@ -62,9 +62,9 @@ python encoder-only-roberta-large/run_getpr_perclient.py \
       --data_dir "$GLUE_DIR" \
       --log_dir ./logs/ \
       --output_dir ./ckpts/ \
-      --max_seq_length 512 \
-      --per_gpu_train_batch_size 8 \
-      --per_gpu_eval_batch_size 8 \
+      --max_seq_length 128 \
+      --per_gpu_train_batch_size 1 \
+      --per_gpu_eval_batch_size 2 \
       --learning_rate $LR \
       --personalization \
       --server_lr $SLR \
@@ -75,7 +75,7 @@ python encoder-only-roberta-large/run_getpr_perclient.py \
       --logging_steps 100 \
       --num_train_epochs $NUMEPOCHS  \
       --warmup_rate 0.06 \
-      --num_prompt_tokens 20 \
+      --num_prompt_tokens 5 \
       --prompt_layer_list None \
       --evaluate_during_training \
       --overwrite_output_dir \
@@ -86,16 +86,14 @@ python encoder-only-roberta-large/run_getpr_perclient.py \
       --local_eps $LOCALEPS &
 
 # By combining the sorted layer indexes from Step 1 and the ratio calculated in Step 2, selected layers for efficient prompt tuning can be obtained
-# e.g.: 24 layers of roberta-large in sorted order: [9, 8, 11, 7, 10, 17, 18, 15, 16, 22, 12, 21, 13, 14, 6, 19, 20, 2, 5, 1, 4, 3, 0]    
-#       ratio calculated in Step 2: 50%
-#       efficient prompt tuning layers for roberta-large model on RTE dataset: [9, 8, 11, 7, 10, 17, 18, 15, 16, 22, 12, 21]
+# e.g.: efficient prompt tuning layers for llama-7b model on MRPC dataset: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 
 
 
 # Step 3: Specify efficient prompt tuning layers with parameter 'prompt_layer_list', and start tuning with the follwoing code
 export CUDA_VISIBLE_DEVICES=0
-python encoder-only-roberta-large/run_fed_pers_with_optim_v2.py \
-    --model_type roberta \
+python decoder-only-llama/run_fed_pers_with_optim_v2.py \
+    --model_type llama \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --task_name $TASK_NAME \
     --do_train \
@@ -105,9 +103,9 @@ python encoder-only-roberta-large/run_fed_pers_with_optim_v2.py \
     --data_dir "$GLUE_DIR" \
     --log_dir ./logs/ \
     --output_dir ./ckpts/ \
-    --max_seq_length 512 \
-    --per_gpu_train_batch_size 8 \
-    --per_gpu_eval_batch_size 8 \
+    --max_seq_length 128 \
+    --per_gpu_train_batch_size 1 \
+    --per_gpu_eval_batch_size 2 \
     --learning_rate $LR \
     --personalization \
     --server_lr $SLR \
@@ -118,8 +116,8 @@ python encoder-only-roberta-large/run_fed_pers_with_optim_v2.py \
     --logging_steps 100 \
     --num_train_epochs $NUMEPOCHS  \
     --warmup_rate 0.06 \
-    --num_prompt_tokens 20 \
-    --prompt_layer_list "[9, 8, 11, 7, 10, 17, 18, 15, 16, 22, 12, 21]" \
+    --num_prompt_tokens 5 \
+    --prompt_layer_list "[8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]" \
     --evaluate_during_training \
     --overwrite_output_dir \
     --data_partition_method "dirichlet_label" \
